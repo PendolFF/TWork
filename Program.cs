db4d681ebcd10b2f1cf3bbd068df85a02c80aa91
PendolFF <morev.ecrgo@gmail.com>
2024-05-05 03:23:33 +0700

    completed the readme file

--- Program.cs
+++ Program.cs
@@ -1,48 +1,40 @@
-﻿class Program
+﻿Console.WriteLine("Введите элементы массивов через запятую:");
+// Считывается введенная строка с консоли и разбивается на массив строк по запятой
+string[] initialArray = Console.ReadLine().Split(',');
+
+// Фильтруется начальный массив строк по длине <= 3
+string[] newArray = FilterArrayByLength(initialArray, 3);
+
+// Выводится на экран новый массив строк
+Console.WriteLine("Новый массив строк с длиной меньше или равной 3 символам:");
+foreach (string str in newArray)
 {
-    static void Main(string[] args)
+    Console.Write($"'{str}'" + " ");
+}
+
+string[] FilterArrayByLength(string[] array, int maxLength)
+{
+    // Подсчитывается количество строк в массиве, удовлетворяющих условию
+    int count = 0;
+    foreach (string str in array)
     {
-        // Пользователю предлагается ввести элементы массивов
-        Console.WriteLine("Введите элементы массивов через запятую:");
-        // Считывается введенная строка с консоли и разбивается на массив строк по запятой
-        string[] initialArray = Console.ReadLine().Split(',');
-
-        // Фильтруется начальный массив строк по длине, оставляя только те строки, которые имеют длину меньше или равную 3 символам
-        string[] newArray = FilterArrayByLength(initialArray, 3);
-
-        // Выводится на экран новый массив строк с длиной меньше или равной 3 символам
-        Console.WriteLine("Новый массив строк с длиной меньше или равной 3 символам:");
-        foreach (string str in newArray)
+        if (str.Length <= maxLength)
         {
-            Console.Write(str + " ");
+            count++;
         }
     }
 
-    static string[] FilterArrayByLength(string[] array, int maxLength)
+    // Создается новый массив из строк подходящей длины
+    string[] result = new string[count];
+    int i = 0;
+    foreach (string str in array)
     {
-        // Подсчитывается количество строк в массиве, удовлетворяющих условию
-        int count = 0;
-        foreach (string str in array)
+        if (str.Length <= maxLength)
         {
-            if (str.Length <= maxLength)
-            {
-                count++;
-            }
+            result[i] = str;
+            i++;
         }
-
-        // Создается новый массив подходящей длины и условию
-        string[] result = new string[count];
-        int index = 0;
-        foreach (string str in array)
-        {
-            if (str.Length <= maxLength)
-            {
-                result[index] = str;
-                index++;
-            }
-        }
-
-        // Возвращается новый массив строк, прошедших фильтрацию
-        return result;
     }
+
+    return result;
 }
